ccd04ed15cb34924014cadcdaab909a193b0dc87
VincentDEJ <dejonckheere.vincent@gmail.com>
2014-10-06 11:59:53 +0200

    Découpage en fonction
    Découpage du code en fonction pour le haut de la fenêtre

--- Flickr/src/flickr/Interface.java
+++ Flickr/src/flickr/Interface.java
@@ -13,21 +13,37 @@ import  java.awt.*;
  * @author Vincent
  */
 public final class Interface extends JFrame{
+    JPanel pan;
     
     public Interface(){
-        super("Flickr");                 //défini le titre
+        //Défini le titre de la fenêtre
+        super("Flickr");                 
         setFenetre();
+        hautFenetre(pan);
     }
     
     protected void setFenetre(){
-        setSize(new Dimension(800,600));        //la taille       
+        //Défini la taille de la fenêtre
+        setSize(new Dimension(800,600));               
 
-        JPanel pan=new JPanel();              //panel 
+        //Création du JPanel
+        pan = new JPanel();               
         
         FlowLayout experimentLayout = new FlowLayout();
         pan.setLayout(experimentLayout);
 
-        JTextField  tf = new JTextField("#Tag"); //un champ texte!
+        pan.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+        //Défini le panel de la JFrame
+        setContentPane(pan);
+        //Affiche la JFrame
+        setVisible(true); 
+        //opération par défaut a la fermeture
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    } 
+    
+    public void hautFenetre(JPanel pan){
+        //Champ texte pour le tag
+        JTextField  tf = new JTextField("#Tag");
         tf.setPreferredSize( new Dimension(500 , 50 ));
         pan.add(tf);                 
 
@@ -39,12 +55,7 @@ public final class Interface extends JFrame{
         //Bouton Favoris
         JButton favoris = new JButton("Favoris");
         favoris.setPreferredSize( new Dimension(100 , 50 ));
-        pan.add(favoris); 
-
-        pan.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-        setContentPane(pan);        //defini le panel de la JFrame
-        setVisible(true);           //affiche la JFrame
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //opération par défaut a la fermeture
-    } 
+        pan.add(favoris);  
+    }
        
 }
